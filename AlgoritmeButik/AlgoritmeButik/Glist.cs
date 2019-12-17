3ffb87db7bee1d8863231547c6a45f257b05371f
Bjarke <bjarkemm@live.dk>
2019-12-17 11:48:06 +0100

    List1.0
    List klassen er implementeret

---
+++ AlgoritmeButik/AlgoritmeButik/Glist.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmeButik
+{
+    class Glist<T> : IEnumerable<T>
+    {
+        private T[] allTheTease;
+        private int count;
+
+        public Glist()
+        {
+            count = 0;
+            allTheTease = new T[4];
+        }
+
+        public void Add(T item)
+        {
+            while(allTheTease.Length <= count)
+            {
+                BiggerBetterStrongFaster();
+            }
+
+            allTheTease[count] = item;
+            count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return allTheTease[i];
+            }
+        }
+
+        private void BiggerBetterStrongFaster()
+        {
+            T[] temp = new T[allTheTease.Length * 2];
+            for (int i = 0; i < allTheTease.Length; i++)
+            {
+                temp[i] = allTheTease[i];
+            }
+            allTheTease = temp;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- AlgoritmeButik/AlgoritmeButik/Program.cs
+++ AlgoritmeButik/AlgoritmeButik/Program.cs
@@ -10,7 +10,14 @@ namespace AlgoritmeButik
     {
         static void Main(string[] args)
         {
+            Glist<int> gList = new Glist<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
+            foreach(int i in gList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
