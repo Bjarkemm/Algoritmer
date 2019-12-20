bbd53d53872cb8812c05ebb4c74d6f87fd0d1909
Tergnos <tergnos@live.dk>
2019-12-20 10:51:05 +0100

    New Test Project
    - Added Unittest project

--- AlgoritmeButik/AlgoritmeButik/Program.cs
+++ AlgoritmeButik/AlgoritmeButik/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlgoritmeButik;
 using AlgoritmeButik.Map;
 
 namespace AlgoritmeButik
---
+++ AlgoritmeButik/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3eb7cd4-d235-4699-a5e0-492a3d40f654")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
